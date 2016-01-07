18e542334c3bc080e70bb002008679652ccaa2c4
12adrianmorente <adry_12_mg@hotmail.com>
2016-01-07 13:53:47 +0100

    Starting over

--- src/NapakalakiGame/CombatResult.java
+++
@@ -1,7 +0,0 @@
-/* encoding: utf-8
- */
-package NapakalakiGame;
-
-public enum CombatResult{
-    WINGAME, WIN, LOSE, LOSEANDCONVERT;
-}
--- src/NapakalakiGame/TreasureKind.java
+++
@@ -1,7 +0,0 @@
-/* encoding: utf-8
- */
-package NapakalakiGame;
-
-public enum TreasureKind {
-    ARMOR,ONEHAND,BOTHHANDS,HELMET,SHOE;
-}
