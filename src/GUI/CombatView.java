42336b301b255018bcf3c9f23ea39c208d924f60
Cerv1 <cervietsiit@correo.ugr.es>
2016-02-09 18:25:50 +0100

    P5

--- src/GUI/CombatView.java
+++ src/GUI/CombatView.java
@@ -60,6 +60,7 @@ public class CombatView extends javax.swing.JPanel {
         L_LOSE = new javax.swing.JLabel();
         L_WINGAME = new javax.swing.JLabel();
         L_WIN = new javax.swing.JLabel();
+        L_WINGAME1 = new javax.swing.JLabel();
 
         L_LOSE.setFont(new java.awt.Font("Comfortaa", 1, 25)); // NOI18N
         L_LOSE.setForeground(new java.awt.Color(207, 16, 16));
@@ -67,21 +68,29 @@ public class CombatView extends javax.swing.JPanel {
 
         L_WINGAME.setFont(new java.awt.Font("Comfortaa", 1, 25)); // NOI18N
         L_WINGAME.setForeground(new java.awt.Color(207, 16, 16));
-        L_WINGAME.setText("¡Has ganado la partida!");
+        L_WINGAME.setText("¡Te conviertes en secatior!");
 
         L_WIN.setFont(new java.awt.Font("Comfortaa", 1, 25)); // NOI18N
         L_WIN.setForeground(new java.awt.Color(207, 16, 16));
         L_WIN.setText("¡Has ganado el combate!");
 
+        L_WINGAME1.setFont(new java.awt.Font("Comfortaa", 1, 25)); // NOI18N
+        L_WINGAME1.setForeground(new java.awt.Color(207, 16, 16));
+        L_WINGAME1.setText("¡Has ganado la partida!");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(L_LOSE)
-            .addComponent(L_WIN)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(L_WINGAME, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(L_LOSE)
+                    .addComponent(L_WIN)
+                    .addComponent(L_WINGAME, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(L_WINGAME1, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -89,8 +98,10 @@ public class CombatView extends javax.swing.JPanel {
                 .addComponent(L_LOSE)
                 .addGap(5, 5, 5)
                 .addComponent(L_WIN)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(L_WINGAME, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(L_WINGAME1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
@@ -100,5 +111,6 @@ public class CombatView extends javax.swing.JPanel {
     private javax.swing.JLabel L_LOSE;
     private javax.swing.JLabel L_WIN;
     private javax.swing.JLabel L_WINGAME;
+    private javax.swing.JLabel L_WINGAME1;
     // End of variables declaration//GEN-END:variables
 }
